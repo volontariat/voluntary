04f16d4d86bbb83b73312bad8f31c30358296490
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-06-04 21:06:11 +0200

    Adds mixin of authorization in voluntary server application client.

--- lib/generators/voluntary/install/templates/app/controllers/application_controller.rb
+++ lib/generators/voluntary/install/templates/app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < Voluntary::ApplicationController
   layout Proc.new { |controller| controller.request.xhr? ? 'facebox' : 'application' }
+  
+  def current_ability
+    @current_ability ||= super.merge(Application::Ability.new(current_user, controller_namespace: current_namespace))
+  end
 end
