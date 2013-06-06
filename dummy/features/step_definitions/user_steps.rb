badb708bb21a9783ee4d697fb3383112651bb663
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-06-07 00:28:36 +0200

    Generate a valid email for user.

--- dummy/features/step_definitions/user_steps.rb
+++ dummy/features/step_definitions/user_steps.rb
@@ -1,4 +1,4 @@
 Given /^a user named "([^\"]*)"$/ do |name|
-  @me = FactoryGirl.create(:user, name: name, email: "#{name}@volontari.at")
+  @me = FactoryGirl.create(:user, name: name, email: "#{name.gsub(' ', '_')}@volontari.at")  
   @me.reload
 end
