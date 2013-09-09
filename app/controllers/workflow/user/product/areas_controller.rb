14dfda6538ae6496d28f060e3f78fbb335ba2dfa
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-09-09 22:06:47 +0200

    Select product in /workflow/user/products/:product_id/areas/:id

--- app/controllers/workflow/user/product/areas_controller.rb
+++ app/controllers/workflow/user/product/areas_controller.rb
@@ -3,5 +3,6 @@ class Workflow::User::Product::AreasController < ApplicationController
     @area = Area.find(params[:id])
     @areas = @area.children_for_product_id(params[:product_id])
     @projects = @area.projects.for_product_id(params[:product_id])
+    @product = Product.find(params[:product_id]) unless params[:product_id] == 'no-name'
   end
 end
