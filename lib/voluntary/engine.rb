9662a6015801bc2bfa3886dea6b0b913df119654
Mathias Gawlista <gawlista.mathias@vimn.com>
2013-10-02 17:46:47 +0200

    Autoload model concerns.

--- lib/voluntary/engine.rb
+++ lib/voluntary/engine.rb
@@ -1,7 +1,8 @@
 module Voluntary
   class Engine < ::Rails::Engine
-    config.autoload_paths << File.expand_path("../..", __FILE__)
+    config.autoload_paths << File.expand_path("../../../app/models/concerns", __FILE__)
     config.autoload_paths << File.expand_path("../../../app/controllers/concerns", __FILE__)
+    config.autoload_paths << File.expand_path("../..", __FILE__)
     config.i18n.load_path += Dir[File.expand_path("../../../config/locales/**/*.{rb,yml}", __FILE__)]
     config.active_record.observers ||= []
     
