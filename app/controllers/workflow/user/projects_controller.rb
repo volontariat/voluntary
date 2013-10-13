81420ff3671ad3807f742f8abc7fa9137dc6b3f2
Mathias Gawlista <gawlista@gmail.com>
2013-10-13 10:30:32 +0200

    Improves project stories collection to consider project and user preferences.

--- app/controllers/workflow/user/projects_controller.rb
+++ app/controllers/workflow/user/projects_controller.rb
@@ -1,6 +1,8 @@
 class Workflow::User::ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
-    @stories = @project.stories.active.paginate(page: params[:page])
+    
+    @stories = @project.product.stories_for_user(current_user).
+    where(project_id: @project.try(:id)).paginate(page: params[:page])
   end
 end
