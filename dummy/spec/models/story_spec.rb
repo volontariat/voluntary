1461e986d4e271d077dcbebece4e237bd60535c3
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2013-10-18 16:55:12 +0200

    Removes page param from Product.stories.

--- dummy/spec/models/story_spec.rb
+++ dummy/spec/models/story_spec.rb
@@ -13,13 +13,13 @@ describe Product do
         
         next_task = subject.next_task_for_user(user)
         next_task.id.should == subject.tasks.first.id
-        Product.stories('no-name', user, 1).count.should == 1
+        Product.stories('no-name', user).count.should == 1
         
         next_task.cancel!
         next_task = subject.next_task_for_user(user)
         next_task.should == nil
         subject.users_without_tasks_ids.include?(user.id).should == true
-        Product.stories('no-name', user, 1).count.should == 0
+        Product.stories('no-name', user).count.should == 0
       end
     end
   end
