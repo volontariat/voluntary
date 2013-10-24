0b34f8ce18c4b3d6121840b26b093638102c3a91
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2013-10-24 19:25:31 +0200

    Only create project user if resource is an user.

--- app/observers/candidature_observer.rb
+++ app/observers/candidature_observer.rb
@@ -8,12 +8,14 @@ class CandidatureObserver < ActiveRecord::Observer
   def after_transition(object, transition)
     case transition.to
       when 'accepted'
-        ProjectUser.find_or_create_by_project_id_and_vacancy_id_and_user_id!(
-          project_id: object.vacancy.project_id, vacancy_id: object.vacancy_id, 
-          user_id: object.user_id
-        )
+        if object.resource_type == 'User'
+          ProjectUser.find_or_create_by_project_id_and_vacancy_id_and_user_id!(
+            project_id: object.vacancy.project_id, vacancy_id: object.vacancy_id, 
+            user_id: object.resource_id
+          )
+        end
         
-        if object.vacancy.limit == object.vacancy.candidatures.accepted.count
+        if object.vacancy.limit.present? && object.vacancy.limit == object.vacancy.candidatures.accepted.count
           object.vacancy.close! unless object.vacancy.closed?
         end
       when 'denied'
