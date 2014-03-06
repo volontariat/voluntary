b6447c63e023e777a05164ff26116c80ff1db277
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-06 17:50:10 +0100

    Accidentally moved db into features. Undo.

--- dummy/features/db/migrate/20130606195337_add_public_attribute_to_roles.voluntary_engine.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from voluntary_engine (originally 20121006162913)
-class AddPublicAttributeToRoles < ActiveRecord::Migration
-  def change
-    add_column :roles, :public, :boolean, default: false
-    add_column :users, :main_role_id, :integer
-  end
-end
