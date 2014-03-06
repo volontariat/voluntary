3edcb830cb8d5560b77380bfc3c39d0086e73b3a
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-06 16:12:50 +0100

    has many candidatures NOT as resource.

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -7,7 +7,7 @@ class Vacancy < ActiveRecord::Base
   belongs_to :resource, polymorphic: true
   belongs_to :project_user
   
-  has_many :candidatures, as: :resource, dependent: :destroy
+  has_many :candidatures, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
   
   accepts_nested_attributes_for :candidatures, allow_destroy: true, reject_if: ->(t) { t['name'].blank? }
