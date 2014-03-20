e983cb0a00cefeeb834bb641518a9deba6e9b821
Mathias Gawlista <gawlista.mathias@vimn.com>
2014-03-20 17:27:31 +0100

    Friendly id upgrade.

--- app/controllers/workflow/user/product/areas_controller.rb
+++ app/controllers/workflow/user/product/areas_controller.rb
@@ -1,8 +1,8 @@
 class Workflow::User::Product::AreasController < ApplicationController
   def show
-    @area = Area.find(params[:id])
+    @area = Area.friendly.find(params[:id])
     @areas = @area.children_for_product_id(params[:product_id])
     @projects = @area.projects.for_product_id(params[:product_id])
-    @product = Product.find(params[:product_id]) unless params[:product_id] == 'no-name'
+    @product = ::Product.find(params[:product_id]) unless params[:product_id] == 'no-name'
   end
 end
--- app/controllers/workflow/user/projects_controller.rb
+++ app/controllers/workflow/user/projects_controller.rb
@@ -1,6 +1,6 @@
 class Workflow::User::ProjectsController < ApplicationController
   def show
-    @project = Project.find(params[:id])
+    @project = Project.friendly.find(params[:id])
     
     @stories = @project.product.stories_for_user(current_user).
     where(project_id: @project.try(:id)).paginate(page: params[:page])
