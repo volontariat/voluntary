8aecc1b75cb1acf99ad16b6fae860ffb5e7aca3a
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-18 21:36:26 +0100

    Improve likes.positive = 1 query if PostgreSQL adapter.

--- app/models/concerns/likeable.rb
+++ app/models/concerns/likeable.rb
@@ -10,7 +10,13 @@ module Likeable
     has_many :dislikers, class_name: 'User', through: :dislikes, source: :user
     
     scope :liked_by, ->(user_id) do
-      select('music_videos.*, likes.created_at AS liked_at').joins('RIGHT JOIN likes ON likes.positive = 1 AND likes.target_type = "MusicVideo" AND likes.target_id = music_videos.id').
+      positive_likes_string = if ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) 
+        'likes.positive = "t"'
+      else
+        'likes.positive = 1'
+      end
+      
+      select('music_videos.*, likes.created_at AS liked_at').joins('RIGHT JOIN likes ON ' + positive_likes_string + ' AND likes.target_type = "MusicVideo" AND likes.target_id = music_videos.id').
       where('likes.user_id = ? AND music_videos.id IS NOT NULL', user_id)
     end
   end
