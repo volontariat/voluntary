63191f2b0a4ae3beb08d2184fb303a0c5dc59499
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-27 19:00:24 +0100

    refs #72 Use rack-cors gem and remove rails-api gem.

--- app/controllers/voluntary/api/v1/base_controller.rb
+++ app/controllers/voluntary/api/v1/base_controller.rb
@@ -5,13 +5,6 @@ module Voluntary
         #include ActionController::Rendering        # enables rendering
         #include ActionController::MimeResponds     # enables serving different content types like :xml or :json
         #include AbstractController::Callbacks      # callbacks for your authentication logic
-        
-        after_filter :set_access_control_headers
-        
-        def set_access_control_headers
-          headers['Access-Control-Allow-Origin'] = '*'
-          headers['Access-Control-Request-Method'] = '*'
-        end
       end
     end
   end
