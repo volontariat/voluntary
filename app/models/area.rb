08306256242f12af7471d716422669136d1cde65
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-03-27 19:00:45 +0100

    refs #77 Workflow product and (user) product area page: consider areas with projects assigned to sub areas deeper than the direct child areas.

--- app/models/area.rb
+++ app/models/area.rb
@@ -6,6 +6,17 @@ class Area < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_and_belongs_to_many :projects
   
+  scope :with_projects_for_product, ->(product_id) do
+    select('DISTINCT(areas.id), areas.*').joins(%Q{
+      LEFT JOIN areas areas2 ON areas2.id = areas.id OR areas2.ancestry like CONCAT(areas.id, '/', '%') 
+        OR areas2.ancestry like CONCAT('%', '/', areas.id) 
+        OR areas2.ancestry like CONCAT('%', '/', areas.id, '/', '%')  
+        OR areas2.ancestry = CONCAT(areas.id, '') 
+      LEFT JOIN areas_projects ON areas_projects.area_id = areas2.id 
+      LEFT JOIN projects ON projects.id = areas_projects.project_id
+    }).where('areas2.id IS NOT NULL AND projects.product_id = ?', product_id)
+  end
+  
   validates :name, presence: true, uniqueness: true
   
   attr_accessible :name, :parent_id
@@ -15,11 +26,13 @@ class Area < ActiveRecord::Base
   friendly_id :name, :use => :slugged
   
   def self.find_by_product_id(product_id)
-    roots.joins(:projects).merge(Project.for_product_id(product_id))
+    #roots.joins(:projects).merge(Project.for_product_id(product_id))
+    roots.with_projects_for_product(product_id)
   end
   
   def children_for_product_id(product_id)
-    children.joins(:projects).merge(Project.for_product_id(product_id))
+    #children.joins(:projects).merge(Project.for_product_id(product_id))
+    children.with_projects_for_product(product_id)
   end
     
   def products
