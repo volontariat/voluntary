b7ccc5d6e05f6830ac83abd9d84a233a7fb99d8e
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-09 13:55:44 +0200

    refs #85 Implements task show page.

--- app/helpers/voluntary/show_helper.rb
+++ app/helpers/voluntary/show_helper.rb
@@ -57,7 +57,8 @@ module Voluntary
       result  = content_tag :dt, raw('&nbsp')
       result += content_tag :dd, render(
         partial: 'shared/resource/actions', locals: { 
-          type: root_model_class_name(resource).tableize, resource: resource, namespace: options[:namespace] 
+          type: root_model_class_name(resource).tableize, resource: resource, namespace: options[:namespace],
+          after_namespace: options[:after_namespace] 
         }
       )
        
