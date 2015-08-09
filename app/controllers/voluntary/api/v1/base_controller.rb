124b96c619bcf7320f82a46adc6c96eaa2882b53
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-09 12:02:30 +0200

    Rescue from access denied and record not found exceptions.

--- app/controllers/voluntary/api/v1/base_controller.rb
+++ app/controllers/voluntary/api/v1/base_controller.rb
@@ -1,7 +1,11 @@
 module Voluntary
   module Api
     module V1
-      class BaseController < ActionController::Base #ActionController::Metal
+      class BaseController < ActionController::Base
+        rescue_from CanCan::AccessDenied, with: :access_denied
+        rescue_from ActiveRecord::RecordNotFound, with: :not_found
+        rescue_from Mongoid::Errors::DocumentNotFound, with: :not_found
+        
         def require_api_key
           render status: 401, json: { error: 'You need to pass your API key for this request!' } if current_user.nil?
         end 
@@ -10,6 +14,16 @@ module Voluntary
         
         def current_user
           @current_user ||= User.where(api_key: params[:api_key]).first
+        end
+        
+        private
+        
+        def access_denied
+          render status: 401, json: { error: 'You are not authorized for this request!' }
+        end
+        
+        def not_found
+          render status: 403, json: { error: 'Record not found! ' + [current_user.id, Story.first].inspect }
         end
       end
     end
