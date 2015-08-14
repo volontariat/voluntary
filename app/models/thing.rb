b14991d3b480fa5841683a57fd851e15de45281c
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-14 20:39:22 +0200

    Decorate own thing suggestion with Wikidata. refs #87

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,3 +1,11 @@
 class Thing < ActiveRecord::Base
   include Concerns::Model::BaseThing
+  
+  def self.suggest(term)
+    known_things = Thing.order(:name).where("name LIKE ?", "%#{term}%").limit(10).map do |t| 
+      { id: t.id, name: t.name }
+    end
+    
+    known_things + Wikidata.search(term, known_things)
+  end
 end
---
+++ app/models/wikidata.rb
@@ -0,0 +1,20 @@
+class Wikidata
+  def self.search(term, known_things)
+    JSON.parse(
+      HTTParty.get(
+        "https://www.wikidata.org/w/api.php?action=wbsearchentities&search=" +
+        "#{URI.encode(term, /\W/)}&format=json&language=en&type=item&continue=0",
+        verify: false
+      ).body
+    )['search'].map do |item|
+      description = if item['description'].to_s.present? && 
+                       item['description'] != 'Wikimedia disambiguation page'
+        " (#{item['description']})"
+      else
+        ''
+      end
+      
+      "#{item['label']}#{description}"
+    end.uniq.select{|i| known_things.select{|t| t[:name] == i }.none? }.map{|item| { name: item } }
+  end
+end
