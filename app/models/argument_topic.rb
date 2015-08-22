223635fb7ae1962b26e1ff127fc8b03ee73cbebb
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-22 15:47:10 +0200

    Implements ArgumentTopic.find_or_create_by_name.

--- app/models/argument_topic.rb
+++ app/models/argument_topic.rb
@@ -4,4 +4,14 @@ class ArgumentTopic < ActiveRecord::Base
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   
   attr_accessible :name, :text
+  
+  def self.find_or_create_by_name(name)
+    resource = where('LOWER(name) = ?', name.to_s.strip.downcase).first
+    
+    if resource
+      resource
+    else
+      resource = create(name: name.to_s.strip)
+    end
+  end
 end
