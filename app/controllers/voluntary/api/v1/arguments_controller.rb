07d34d0c1ba60c3cd5a0b4f4170db85fac3e8fa4
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-22 15:46:29 +0200

    Implements api/v1/arguments#update and #destroy.

--- app/controllers/voluntary/api/v1/arguments_controller.rb
+++ app/controllers/voluntary/api/v1/arguments_controller.rb
@@ -31,7 +31,43 @@ module Voluntary
           raise CanCan::AccessDenied if current_user.blank?
           
           respond_to do |format|
-            format.json { render json: Argument.create_with_topic(params[:argument]) }
+            format.json { render json: Argument.create_with_topic(current_user.id, params[:argument]) }
+          end
+        end
+        
+        def update
+          resource = current_user.arguments.where('arguments.id = ?', params[:id]).first
+          errors = {}
+          
+          if resource.nil?
+            errors[:base] = I18n.t('activerecord.errors.models.general.attributes.base.user_resource_not_found')
+          else
+            topic = ArgumentTopic.find_or_create_by_name params[:argument][:topic_name]
+            params[:argument][:topic_id] = topic.id
+            resource.update_attributes params[:argument]
+            resource.valid?
+            errors = resource.errors.to_hash
+          end
+          
+          respond_to do |format|
+            format.json do
+              render json: errors.empty? ? resource : { errors: errors }
+            end
+          end
+        end
+        
+        def destroy
+          resource = current_user.arguments.find params[:id]
+          resource.destroy
+          
+          respond_to do |format|
+            format.json do
+              render json: if resource.persisted?
+                { error: I18n.t('activerecord.errors.models.argument.attributes.base.deletion_failed') }
+              else
+                {}
+              end
+            end
           end
         end
       end
