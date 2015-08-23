396d16df58cbe9c3591c179ea6df9d3633924f5e
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-23 20:09:58 +0200

    Adds likes count columns to arguments.

---
+++ db/migrate/20150823174056_add_likes_count_to_arguments.rb
@@ -0,0 +1,6 @@
+class AddLikesCountToArguments < ActiveRecord::Migration
+  def change
+    add_column :arguments, :likes_count, :integer, default: 0
+    add_column :arguments, :dislikes_count, :integer, default: 0
+  end
+end
