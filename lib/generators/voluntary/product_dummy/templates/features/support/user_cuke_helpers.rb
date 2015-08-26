645810ab8a817593d2ee93e1e832798d2a737e4b
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-08-26 12:19:52 +0200

    Fixes cucumbers.

--- lib/generators/voluntary/product_dummy/templates/features/support/user_cuke_helpers.rb
+++ lib/generators/voluntary/product_dummy/templates/features/support/user_cuke_helpers.rb
@@ -4,7 +4,7 @@ module UserCukeHelpers
   # and the given override attributes, adds the standard aspects to it
   # and returns it
   def create_user(overrides={})
-    Factory(
+    FactoryGirl.create(
       :user, {
         password: 'password',
         password_confirmation: 'password'
@@ -16,13 +16,13 @@ module UserCukeHelpers
   def login_as(user)
     fill_in 'Email', with: "#{user}@volontari.at"
     fill_in 'Password', with: 'password'
-    click_button :submit
+    click_button 'Log in'
   end
 
   # create a new @me user, if not present, and log in using the
   # integration_sessions controller (automatic)
   def automatic_login
-    @me ||= Factory(:user)
+    @me ||= FactoryGirl.create(:user)
     page.driver.visit(new_integration_sessions_path(user_id: @me.slug))
     click_button "Login"
   end
@@ -31,7 +31,7 @@ module UserCukeHelpers
   def manual_login
     visit login_page
     login_as @me.name
-    visit user_confirmation_path(confirmation_token: @me.confirmation_token)  
+    #visit user_confirmation_path(confirmation_token: @me.confirmation_token)  
   end
 
   # checks the page content to see, if the login was successful
