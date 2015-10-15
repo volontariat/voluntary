1193525d5e5d28093ae8e8aea97eee10ca9e0b67
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-10-15 18:23:49 +0200

    refs #39 migrate nav-list to list-group.

--- lib/vendors/simple_navigation/renderer/twitter_sidenav.rb
+++ lib/vendors/simple_navigation/renderer/twitter_sidenav.rb
@@ -21,12 +21,10 @@ module Vendors
               options.merge!(method: item.method, confirm: I18n.t('general.questions.are_you_sure'))
             end
             
-            content += content_tag :li, link_to(item.name, item.url, options), class: klass
+            content += link_to(item.name, item.url, options.merge({ class: "list-group-item #{klass}"}))
           end
       
-          ul = content_tag :ul, content, class: 'nav nav-list'
-      
-          content_tag :div, ul, class: 'well sidebar-nav'
+          content_tag :div, content_tag(:div, content, class: 'list-group'), class: 'well sidebar-nav'
         end
       end
     end
