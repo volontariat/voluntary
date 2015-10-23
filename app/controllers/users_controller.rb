8ee8c429905586001c8a3157d5df4dfb26ab9fc4
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-10-23 12:51:42 +0200

    refs #97 Use resource variable in users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,24 +18,31 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user.attributes = params[:user] if params[:user].present?
   end
   
   def preferences
-    if params[:user] && current_user.update_attributes(params[:user])
-      redirect_to preferences_user_path(current_user), notice: t('general.form.successfully_updated') and return
+    raise CanCan::AccessDenied if !current_user || (current_user.id != @user.id && !current_user.roles?(:master))
+    
+    if params[:user] && @user.update_attributes(params[:user])
+      redirect_to preferences_user_path(@user), notice: t('general.form.successfully_updated') and return
     end
   end
   
   def update
-    if current_user.update_attributes(params[:user])
-      redirect_to edit_user_path(current_user), notice: t('general.form.successfully_updated')
+    raise CanCan::AccessDenied if !current_user || (current_user.id != @user.id && !current_user.roles?(:master))
+    
+    if @user.update_attributes(params[:user])
+      redirect_to edit_user_path(@user), notice: t('general.form.successfully_updated')
     else
       render :edit
     end
   end
 
   def destroy
-    current_user.destroy
+    raise CanCan::AccessDenied if !current_user || (current_user.id != @user.id && !current_user.roles?(:master))
+    
+    @user.destroy
     redirect_to users_url, notice: t('general.form.destroyed')
   end
   
