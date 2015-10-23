5b037c834ed54e5a8d3d623138e8304b46190c3a
Mathias Gawlista <gawlista.mathias@vimn.com>
2015-10-23 12:51:25 +0200

    refs #94 Partly rewrite of unused tasks controller and view.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,19 @@
 class TasksController < ApplicationController
   include Applicat::Mvc::Controller::Resource
-  include Wizard::Controller
-  
-  wizard_steps :setup_tasks, :activate
   
   before_filter :build_resource, only: [:new, :create]
-  before_filter :resource, only: [:show, :edit, :update, :setup_tasks, :activate]
+  before_filter :resource, only: [:show, :edit, :update, :destroy]
   
   load_and_authorize_resource
   
   respond_to :html, :js, :json
   
   def index
-    parent = find_parent Task::PARENT_TYPES
-    @tasks = parent ? parent.tasks : Task.all
+    @story = Story.find(params[:story_id])
+    @project = @story.project
+    @tasks = @story.custom_tasks
+    @tasks = @tasks.paginate(page: params[:page], per_page: 1)
+    @twitter_sidenav_level = 5
   end
   
   def show
@@ -21,48 +21,36 @@ class TasksController < ApplicationController
   end
   
   def new
-    render_wizard
   end
   
   def create
-    if @task.initialization
-      redirect_to edit_task_path(@task) and return
+    if @task.save
+      redirect_to story_tasks_path(@story), notice: t('tasks.create.successful')
     else
-      render_wizard
+      render 'new'
     end
   end
   
   def edit
-    render_wizard
   end
   
   def update
-    @task.attributes = params[:task]
-    
-    # shift the first empty task set after initialization on state :initialized
-    @task.tasks.shift if params[:next_step] == '1' && !@task.tasks.first.valid?
-    success = params[:next_step] == '1' ? @task.send(step) : @task.save
-      
-    if success
+    if @task.update(params[:task])
       redirect_to(
-        edit_task_path(@task), notice: t('general.form.successfully_updated')
+        edit_task_path(@task), notice: t('tasks.update.successful')
       )
-    else
-      render_wizard
     end
-    
-    return
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    redirect_to @task.story, notice: t('general.form.destroyed')
+    redirect_to story_tasks_path(@story), notice: t('general.form.destroyed')
   end
   
   def resource
     @task = Task.find(params[:id]) unless @task || !params[:id].present?
     @story = @task.story unless @story || !@task
+    @project = @story.project
     @task
   end
   
@@ -76,13 +64,7 @@ class TasksController < ApplicationController
   
   def build_resource
     @story = find_parent Task::PARENT_TYPES, action_name == 'create' ? :task : nil
+    @project = @story.project
     @task = @story.tasks.new({ story_id: @story.id }.merge(params[:task] || {}))
-  end
-  
-  def render_wizard
-    @presenter = Resources::General::Wizards::TaskPresenter.new(
-      self.view_context, resource: resource
-    )
-    render 'general/wizard'
   end
 end
